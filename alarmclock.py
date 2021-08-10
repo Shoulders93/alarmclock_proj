cf93c0d48a18a078ac2215eb5f5f014146a80d29
Kory Attleson <kory.attleson@gmail.com>
2021-08-10 12:59:36 -0500

    Got the alarm clock to loop until it hits the time you want for alarm and then alarm goes off

--- alarmclock.py
+++ alarmclock.py
@@ -11,13 +11,12 @@ class Alarmclock:
         self.time_alarm_set = time_alarm_set
 
     def trigger_alarm(self):
-        if (self.alarm_on == False):
-            print('Your alarm has not gone off yet')
-        elif (self.current_time == self.time_alarm_set):
-            self.alarm_on == True
-            print("You're alarm is going off!")
-
-
-
-
-
+        while (self.alarm_on == False):
+            if (self.current_time == self.time_alarm_set):
+                self.alarm_on = True
+                print(self.current_time)
+                print("You're alarm is going off!")
+            elif (self.alarm_on == False):
+                print(self.current_time)
+                self.current_time = self.current_time + 1
+                print('Your alarm has not gone off yet')
--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from alarmclock import Alarmclock
 
 first_alarm_clock = Alarmclock()
 
-first_alarm_clock.set_time_and_alarm(8,8)
+first_alarm_clock.set_time_and_alarm(6,8)
 first_alarm_clock.trigger_alarm()
