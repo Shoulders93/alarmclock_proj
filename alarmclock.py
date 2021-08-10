a2a5ffc4dfacef8ddca9fa99e1b27f8d61059846
Kory Attleson <kory.attleson@gmail.com>
2021-08-10 12:34:51 -0500

    created object and method for alarm clock

--- alarmclock.py
+++ alarmclock.py
@@ -1,9 +1,15 @@
 class Alarmclock:
-    def __init__(self, current_time, alarm_time):
+    def __init__(self, current_time, set_alarm):
         self.current_time = current_time
-        self.alarm_on = []
-        self.alarm_off = []
-        self.time_alarm_set = alarm_time
+        self.alarm_on = False
+        self.alarm_off = True
+        self.time_alarm_set = set_alarm
 
-    def alarm(self):
-        self.change_current_time += 1
+
+    def trigger_alarm(self):
+        if (self.alarm_on == False):
+            self.current_time += 1
+            print('Your alarm has not gone off yet')
+        elif (self.current_time == self.time_alarm_set):
+            self.alarm_on == True
+            print("You're alarm is going off!")
--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from alarmclock import Alarmclock
 
-first_alarm_clock = Alarmclock(7, 8)
+first_alarm_clock = Alarmclock(6, 8)
 
 
-print(first_alarm_clock.current_time)
-first_alarm_clock.change_current_time()
-print(first_alarm_clock.current_time)
+first_alarm_clock()
